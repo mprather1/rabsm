5b1c9727dd74338a5f06f0ee5be7fd403ef6a712
Michael Prather <mprather1@gmail.com>
2016-05-03 19:05:35 -0500

    application index and lib files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+// This is a manifest file that'll be compiled into app.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
@@ -11,6 +11,10 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require ./app/lib/underscore
+//= require ./app/lib/backbone
+//= require_tree ./app/models
+//= require_tree ./app/collections
+//= require_tree ./app/views
+//= require_tree ./app/routers
+//= require ./app/main
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "users" => "users#index"
   get "user/:id" => "users#show"
+  root to: 'application#index'
 end
